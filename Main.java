f83bf685922ec59f52f422798d705fd1f4e4bff9
ManzMa <55273967+ManzMa@users.noreply.github.com>
2020-07-13 14:12:01 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("UI.fxml"));
+			Parent root = loader.load();
+			UI_Controller controller = loader.getController();
+			primaryStage.setScene(new Scene(root));
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ UI_Controller.java
@@ -0,0 +1,12 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class UI_Controller {
+	@FXML
+	Button Speichern;
+	public void buttonPressed() {
+		
+	}
+}
